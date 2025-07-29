c502a2f6b1adff55a19b6e92afb8009538a4d0b1
rirufa <test@yahoo.co.jp>
2025-07-29 16:08:49 +0900

    圧縮したものを展開するようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-vue';
+import viteCompression from  "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+  plugins: [plugin(), viteCompression({
+    include: /\.(html|xml|css|json|js|mjs|svg|yaml|yml|toml|kml|csv|geo_cache)$/,
+    algorithms: ['gzip']
+  })],
     server: {
         port: 53198,
     }
